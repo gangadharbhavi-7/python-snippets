c3b6718b1b39cf82db8384a9f67b783766191943
Gangadhar B <gangadharbavi312004@gmail.com>
2024-12-13 22:58:57 +0530

    Update

---
+++ login.py
@@ -0,0 +1,11 @@
+saved_name="gangadhar"
+saved_password ="horse"
+name= "enter your name:"
+if saved_name==name:
+    print("Hi!",name)
+    password ="enter your password: "
+    if saved_password== password:
+        print("access granted!!")
+    else:
+        print(
+
