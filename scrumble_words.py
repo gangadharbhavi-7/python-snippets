2af579698040bd8a897cc0cf9934500319240555
Gangadhar B <gangadharbavi312004@gmail.com>
2025-02-03 22:43:30 +0530

    Update

---
+++ scrumble_words.py
@@ -0,0 +1,11 @@
+import random
+
+def scramble_word(word):
+    if len(word) > 3:
+        middle = list(word[1:-1])
+        random.shuffle(middle)
+        return word[0] + ''.join(middle) + word[-1]
+    return word
+
+word = input("Enter a word: ")
+print("Scrambled word:", scramble_word(word))
